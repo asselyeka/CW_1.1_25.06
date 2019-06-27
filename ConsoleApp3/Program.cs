117deb82b80a8bdb4e9faa480ebe840c00a66d60
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-27 11:38:06 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 	вычисления значения функции y=7x^2-3x+4  при любом значении x;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Введите число х, чтобы вычислить значение функции y=7x^2-3x+4:");
+            string textAsNum = Console.ReadLine();
+            x = int.Parse(textAsNum);
+            y = 7 * x * x - 3 * x + 4;
+            Console.Write("y=");
+            Console.Write(y);
+            Console.ReadKey();
+        }
+    }
+}
